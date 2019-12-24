5ea1324b211a80210357808f12ddf2c4ab39b716
Hoan <hoantdbhaf180148@example.com>
2019-12-24 11:34:43 +0700

    abc

---
+++ index.php
@@ -0,0 +1,14 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>welcome to my web</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	
+	<?php echo "xin chao!" ?>
+	
+</body>
+</html>
